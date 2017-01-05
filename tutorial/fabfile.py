d1c1bde66cd8b5ddde4b481582b15669b6e1ce02
ankitaneosoft <ankita.lahoti@wwindia.com>
2017-01-05 15:06:34 +0530

    deploying changes

--- tutorial/fabfile.py
+++ tutorial/fabfile.py
@@ -1,6 +1,17 @@
-from fabric.api import local
+from fabric.api import local, settings, run, cd
 
 def prepare_deploy():
     local("./manage.py test quickstart")
     local("git add -A && git commit -m 'deploying changes'")
     local("git push")
+
+
+def deploy():
+    code_dir = '/home/neosoft/Demo'
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            run("git clone https://github.com/ankitaneosoft/rest_api_prj.git %s" % code_dir)
+    with cd(code_dir):
+    	#run("git clone https://github.com/ankitaneosoft/rest_api_prj.git %s" % code_dir)
+        run("git pull origin master")
+        run("touch app.wsgi") 
