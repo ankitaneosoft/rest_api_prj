4ea4695609bc370772b973c5befa9fe51eb5854d
ankitaneosoft <ankita.lahoti@wwindia.com>
2017-01-05 17:53:29 +0530

    deploying changes

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import local, settings, run, cd,sudo
 
 def prepare_deploy():
-    local("/tutorial/manage.py test quickstart")
+    local("tutorial/manage.py test quickstart")
     local("git add -A && git commit -m 'deploying changes'")
     local("git push")
 
