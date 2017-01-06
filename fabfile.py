40ce5913e4ac08daee0b8b47154296fad0cfaabf
ankitaneosoft <ankita.lahoti@wwindia.com>
2017-01-06 16:31:54 +0530

    deploying changes

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,8 @@
 from fabric.api import local, settings, run, cd,sudo,env
 #either define this or system will prompt for ip.
 
-#env.hosts = ['10.0.30.109']
-env.hosts = ['webwerks@10.0.8.104']
+env.hosts = ['neosoft@10.0.30.109']
+#env.hosts = ['webwerks@10.0.8.104']
 
 
 def prepare_deploy():
